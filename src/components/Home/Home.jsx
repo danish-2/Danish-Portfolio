096fb6b2cd879619ab368f069db37be7dc9a605c
danish-2 <mohammeddanishkhan98@gmail.com>
2023-10-01 02:56:38 +0530

    some changes

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
+import "./home.css"
+import Social from './Social'
+import Data from './Data'
+import ScrollDown from './ScrollDown'
 
-export default function   () {
+export default function Home  () {
   return (
     <section className="home section" id="home">
         <div className="home__container container grid">
           <div className="home__content grid">
-            
+            <Social/>
+
+            <div className="home__img"></div>
+
+            <Data/>
           </div>
+          <ScrollDown/>
         </div>
     </section>
   )
---
+++ src/components/Home/Social.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Social() {
+  return (
+      <div className="home__social">
+        <a href="https://www.instagram.com/_ig_danish_7/" 
+        className="home__social-icon" target="_blank">
+            <i className='uil uil-instagram'></i>
+        </a>
+        <a href="" className="home__social-icon" target="_blank">
+            <i className='uil uil-dribbble'></i>
+        </a>
+        <a href="" className="home__social-icon" target="_blank">
+            <i className="uil uil-github-alt"></i>
+        </a>
+      </div>
+    )
+}
